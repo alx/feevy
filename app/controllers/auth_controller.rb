13a0ed63a22513f0007fc2765504c829f50bac6c
feevy <feevy@2117fd12-192b-0410-bd5d-90befd50b342>
2007-03-13 15:15:09 +0000

    Change redirections to redirect users to manage controller
    git-svn-id: svn+ssh://64.22.96.6/var/www/apps/feevy/repos/trunk@33 2117fd12-192b-0410-bd5d-90befd50b342

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -242,7 +242,7 @@ class AuthController < ApplicationController
   				end  
           self.saveSession @newuser
           flash[:message] = "Your account is confirmed. Please take some time to setup your preferences."
-          redirect_to :action => "info"
+          redirect_to :controller => "manage"
   				#Notification.deliver_signup(@newuser, @app)
   				#cookies[:email] = { :value => @newuser.email, :expires => nil }
           #flash['message'] = "We have sent a message to #{@newuser.email}. "
