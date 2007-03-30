fc274ec5f3f3856eec77ae9c4ebe66f1155efd64
feevy <feevy@2117fd12-192b-0410-bd5d-90befd50b342>
2007-03-30 04:07:08 +0000

    Remove feevy helper. movee method to app helper.
    git-svn-id: svn+ssh://64.22.96.6/var/www/apps/feevy/repos/trunk@74 2117fd12-192b-0410-bd5d-90befd50b342

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,24 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  # limitamos el texto de una entrada a 
+  def abbreviate(text, limit)
+    unless text.size > limit
+      strip_html(text)[0..limit] + '(...)'
+    else
+      strip_html(text)[0..limit] 
+    end
+  end
+  
+  def strip_html(line) 
+      line.gsub(/\n/, ' ').gsub(/<.*?>/, '') 
+  end
+  
+  def safe_truncate(text, limit, truncate_string = "...")
+  end
+  
+  def url_escape(string)
+    "');document.write(unescape('" << string.gsub(/([^ a-zA-Z0-9_.-]+)/n) do
+      '%' + $1.unpack('H2' * $1.size).join('%').upcase
+    end << "'));document.write('"
+  end
 end
