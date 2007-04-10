535ba8c0b0f0d88361a50138df248afcb6df8078
feevy <feevy@2117fd12-192b-0410-bd5d-90befd50b342>
2007-04-10 06:18:52 +0000

    Eliminate null values
    git-svn-id: svn+ssh://64.22.96.6/var/www/apps/feevy/repos/trunk@103 2117fd12-192b-0410-bd5d-90befd50b342

--- app/controllers/ping_controller.rb
+++ app/controllers/ping_controller.rb
@@ -13,8 +13,8 @@ class PingController < ApplicationController
     @ping = Ping.create(:name => "Master Ping", :current_offset => 0) if @ping.nil?
     # Get list of feeds to send to client, depending on master ping offset
     @feeds = Feed.find(:all, :limit => nb_feeds_in_list, :offset => @ping.current_offset)
-    @feeds = Feed.find(:all, :conditions => ["feeds.id in (?)", @feeds], :include => [:latest_post], :order => 'feeds.id, posts.created_at')
-    @feeds.delete_if {|feed| feed.link.nil?}
+    @feeds = Feed.find(:all, :conditions => ["feeds.id in (?)", @feeds], :include => :latest_post, :order => 'feeds.id, posts.created_at')
+    @feeds.delete_if {|feed| feed.link.nil? or feed.latest_post.nil? }
     # Check offset, and reset it if greater than feed list size
     new_offset = @ping.current_offset + nb_feeds_in_list
     if new_offset > Feed.count
