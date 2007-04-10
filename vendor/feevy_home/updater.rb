b457a8ef8a0de625a4879259f77f7bd516501544
feevy <feevy@2117fd12-192b-0410-bd5d-90befd50b342>
2007-04-10 16:41:32 +0000

    Manage more erros
    git-svn-id: svn+ssh://64.22.96.6/var/www/apps/feevy/repos/trunk@106 2117fd12-192b-0410-bd5d-90befd50b342

--- vendor/feevy_home/updater.rb
+++ vendor/feevy_home/updater.rb
@@ -7,32 +7,38 @@ SERVER = "www.feevy.com"
 
 puts "Starting updates..."
 while true
-  doc = Hpricot(open("http://#{SERVER}/ping/list"), :xml => true)
-  (doc/:feed).each do |feed|
-    feed_id   = feed.at('id').innerHTML
-    feed_rss  = feed.at('rss').innerHTML
-    feed_post = feed.at('post').innerHTML
-    puts "Feed #{feed_id}: #{feed_rss}"
-    # Read feed rss
-    begin
-      dist = Hpricot(open(feed.at('rss').innerHTML), :xml => true)
-      item = dist.search("item:first|entry:first")
-      # Get first post url
-      link = item.search("link:first")
-      unless link.nil?
-        post_url = link.text
-        post_url = link.to_s.scan(/href=['"]?([^'"]*)['" ]/).to_s if (post_url.nil? or post_url.empty?)
+  begin
+    doc = Hpricot(open("http://#{SERVER}/ping/list"), :xml => true)
+    (doc/:feed).each do |feed|
+      feed_id   = feed.at('id').innerHTML
+      feed_rss  = feed.at('rss').innerHTML
+      feed_post = feed.at('post').innerHTML
+      puts "Feed #{feed_id}: #{feed_rss}"
+      # Read feed rss
+      begin
+        dist = Hpricot(open(feed.at('rss').innerHTML), :xml => true)
+        item = dist.search("item:first|entry:first")
+        # Get first post url
+        link = item.search("link:first")
+        unless link.nil?
+          post_url = link.text
+          post_url = link.to_s.scan(/href=['"]?([^'"]*)['" ]/).to_s if (post_url.nil? or post_url.empty?)
+        end
+        puts "Old: #{feed_post}"
+        puts "New: #{post_url}"
+        # If url not the same, ping server
+        if post_url != feed_post then
+          puts "pinging #{feed_id}..."
+          open("http://#{SERVER}/ping/update/#{feed_id}")
+        end
+      rescue Timeout::Error
+        puts "Timeout on this feed"
+      rescue => err
+        puts "Error while reading this feed: #{err}"
       end
-      puts "Old: #{feed_post}"
-      puts "New: #{post_url}"
-      # If url not the same, ping server
-      if post_url != feed_post then
-        puts "pinging #{feed_id}..."
-        open("http://#{SERVER}/ping/update/#{feed_id}")
-      end
-    rescue => err
-      puts "Error while reading this feed: #{err}"
     end
+  rescue
+    "Error on this cycle"
   end
   puts "Pinging cycle finished, restarting it"
   puts "===="
