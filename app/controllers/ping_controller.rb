675619edef1637d856157fa68ee5948983096353
feevy <feevy@2117fd12-192b-0410-bd5d-90befd50b342>
2007-04-11 20:23:58 +0000

    Add lock on server
    git-svn-id: svn+ssh://64.22.96.6/var/www/apps/feevy/repos/trunk@113 2117fd12-192b-0410-bd5d-90befd50b342

--- app/controllers/ping_controller.rb
+++ app/controllers/ping_controller.rb
@@ -7,23 +7,32 @@ class PingController < ApplicationController
   end
   
   def list
-    nb_feeds_in_list = 100
     # Get master Ping offset or create it if nil
     @ping = Ping.find(:first)
     @ping = Ping.create(:name => "Master Ping", :current_offset => 0) if @ping.nil?
-    current_offset = @ping.current_offset
-    # Check offset, and reset it if greater than feed list size
-    new_offset = current_offset + nb_feeds_in_list
-    if new_offset > Feed.count
-      @ping.update_attribute("current_offset", 0)
-    else
-      @ping.update_attribute("current_offset", new_offset)
-    end
     
-    # Get list of feeds to send to client, depending on master ping offset
-    @feeds = Feed.find(:all, :limit => nb_feeds_in_list, :offset => current_offset)
-    @feeds = Feed.find(:all, :conditions => ["feeds.id in (?)", @feeds], :include => :latest_post, :order => 'feeds.id, posts.created_at')
-    @feeds.delete_if {|feed| feed.link.nil? or feed.latest_post.nil? }
-    render :layout => false
+    # If ping server is locked, tell client to wait
+    if @ping.lock == 1 then
+      render :layout => false, :status => 503
+    else
+      @ping.update_attribute("lock", 1)
+      current_offset = @ping.current_offset
+      # Check offset, and reset it if greater than feed list size
+    
+      nb_feeds_in_list = 100
+      new_offset = current_offset + nb_feeds_in_list
+      if new_offset > Feed.count
+        @ping.update_attribute("current_offset", 0)
+      else
+        @ping.update_attribute("current_offset", new_offset)
+      end
+    
+      # Get list of feeds to send to client, depending on master ping offset
+      @feeds = Feed.find(:all, :limit => nb_feeds_in_list, :offset => current_offset)
+      @feeds = Feed.find(:all, :conditions => ["feeds.id in (?)", @feeds], :include => :latest_post, :order => 'feeds.id, posts.created_at')
+      @feeds.delete_if {|feed| feed.link.nil? or feed.latest_post.nil? }
+      @ping.update_attribute("lock", 0)
+      render :layout => false
+    end
   end
 end
