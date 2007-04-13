ffe730927f5cdc0288cc48301135515167054a4e
feevy <feevy@2117fd12-192b-0410-bd5d-90befd50b342>
2007-04-13 11:59:08 +0000

    Improve dot_file algo
    git-svn-id: svn+ssh://64.22.96.6/var/www/apps/feevy/repos/trunk@125 2117fd12-192b-0410-bd5d-90befd50b342

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -28,7 +28,18 @@ class WelcomeController < ApplicationController
   end
   
   def dot_file
-    @subscriptions = Subscription.find(:all)
+    now = DateTime::now()
+    # Generate cache_key
+    cache_key = "dot_file_#{now.yday()}_#{now.year()}"
+
+    # Get entries from cache or generate entries if not found
+    unless @subscriptions = CACHE.get(cache_key)
+      @subscriptions = Subscription.find(:all)
+      @subscriptions.delete_if do |sub|
+        sub.user_id.nil? or sub.feed_id.nil? or (sub.user.subscriptions.size == 1 and sub.feed.subscriptions.size == 1)
+      end
+      CACHE.set(cache_key, @entries, 60*60*24)
+    end
     render :layout => false
   end
 end
