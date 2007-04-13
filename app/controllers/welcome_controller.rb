199f4a9dece0a5d8e86ed1919378bebba542dfe8
feevy <feevy@2117fd12-192b-0410-bd5d-90befd50b342>
2007-04-13 12:13:27 +0000

    Back to basics
    git-svn-id: svn+ssh://64.22.96.6/var/www/apps/feevy/repos/trunk@127 2117fd12-192b-0410-bd5d-90befd50b342

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -28,18 +28,7 @@ class WelcomeController < ApplicationController
   end
   
   def dot_file
-    now = DateTime::now()
-    # Generate cache_key
-    cache_key = "dot_file_#{now.yday()}_#{now.year()}"
-
-    # Get entries from cache or generate entries if not found
-    unless @subscriptions = CACHE.get(cache_key)
-      @subscriptions = Subscription.find(:all)
-      @subscriptions.delete_if do |sub|
-        sub.user_id.nil? or sub.feed_id.nil? or (sub.user.subscriptions.size == 1 and sub.feed.subscriptions.size == 1)
-      end
-      CACHE.set(cache_key, @subscriptions, 60*60*24)
-    end
+    @subscriptions = Subscription.find(:all)
     render :layout => false
   end
 end
