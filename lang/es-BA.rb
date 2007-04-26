ff9de96be9cb869fc8c2bfde8ee5f7e9a0355892
feevy <feevy@2117fd12-192b-0410-bd5d-90befd50b342>
2007-04-26 17:09:47 +0000

    Add basque translation
    git-svn-id: svn+ssh://64.22.96.6/var/www/apps/feevy/repos/trunk@149 2117fd12-192b-0410-bd5d-90befd50b342

---
+++ lang/es-BA.rb
@@ -0,0 +1,6 @@
+# es-ES lang file
+Localization.define('es-ES') do |l|
+  l.store "read_more",    "gehiago irakurri"
+  l.store "free_service", "Feevy zerbitzu librea da"
+  l.store "get_yours",    "eskuratu zure iturriak"
+end
