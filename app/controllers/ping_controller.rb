f03badd9833b42798c18c3b29ce8d7b8f0c7e69f
feevy <feevy@2117fd12-192b-0410-bd5d-90befd50b342>
2007-04-27 11:08:34 +0000

    Replace ping controller
    git-svn-id: svn+ssh://64.22.96.6/var/www/apps/feevy/repos/trunk@152 2117fd12-192b-0410-bd5d-90befd50b342

--- app/controllers/ping_controller.rb
+++ app/controllers/ping_controller.rb
@@ -1,47 +1,38 @@
 class PingController < ApplicationController
   
   def update
-    if true
-      render :nothing => true, :status => 503
-    else
-      feed = Feed.find(params[:id])
-      feed.refresh
-      render :nothing => true
-    end
+    feed = Feed.find(params[:id])
+    feed.refresh
+    render :nothing => true
   end
   
   def list
+    # Get master Ping offset or create it if nil
+    @ping = Ping.find(:first)
+    @ping = Ping.create(:name => "Master Ping", :current_offset => 0) if @ping.nil?
     
-    if true
-      render :nothing => true, :status => 503
+    # If ping server is locked, tell client to wait
+    if @ping.lock == 1 then
+      render :layout => false, :status => 503
     else
-      # Get master Ping offset or create it if nil
-      @ping = Ping.find(:first)
-      @ping = Ping.create(:name => "Master Ping", :current_offset => 0) if @ping.nil?
+      @ping.update_attribute("lock", 1)
+      current_offset = @ping.current_offset
+      # Check offset, and reset it if greater than feed list size
     
-      # If ping server is locked, tell client to wait
-      if @ping.lock == 1 then
-        render :layout => false, :status => 503
+      nb_feeds_in_list = 100
+      new_offset = current_offset + nb_feeds_in_list
+      if new_offset > Feed.count
+        @ping.update_attribute("current_offset", 0)
       else
-        @ping.update_attribute("lock", 1)
-        current_offset = @ping.current_offset
-        # Check offset, and reset it if greater than feed list size
-    
-        nb_feeds_in_list = 100
-        new_offset = current_offset + nb_feeds_in_list
-        if new_offset > Feed.count
-          @ping.update_attribute("current_offset", 0)
-        else
-          @ping.update_attribute("current_offset", new_offset)
-        end
-    
-        # Get list of feeds to send to client, depending on master ping offset
-        @feeds = Feed.find(:all, :limit => nb_feeds_in_list, :offset => current_offset)
-        @feeds = Feed.find(:all, :conditions => ["feeds.id in (?)", @feeds], :include => :latest_post, :order => 'feeds.id, posts.created_at')
-        @feeds.delete_if {|feed| feed.link.nil? or feed.latest_post.nil? }
-        @ping.update_attribute("lock", 0)
-        render :layout => false
+        @ping.update_attribute("current_offset", new_offset)
       end
+    
+      # Get list of feeds to send to client, depending on master ping offset
+      @feeds = Feed.find(:all, :limit => nb_feeds_in_list, :offset => current_offset)
+      @feeds = Feed.find(:all, :conditions => ["feeds.id in (?)", @feeds], :include => :latest_post, :order => 'feeds.id, posts.created_at')
+      @feeds.delete_if {|feed| feed.link.nil? or feed.latest_post.nil? }
+      @ping.update_attribute("lock", 0)
+      render :layout => false
     end
   end
   
