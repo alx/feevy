cff1a04a57feffc6f899fc9c35f25da0d9553de5
wwwfeev <wwwfeev@2117fd12-192b-0410-bd5d-90befd50b342>
2007-06-16 11:23:39 +0000

    Cache all welcome variables
    git-svn-id: svn+ssh://64.22.96.6/var/www/apps/feevy/repos/trunk@200 2117fd12-192b-0410-bd5d-90befd50b342

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -14,7 +14,11 @@ class WelcomeController < ApplicationController
   end
   
   def welcome
-    @feeds = Feed.find(:all, :limit => 20, :order => "created_at DESC")
+    
+    unless @feeds = CACHE.get("welcome_feeds")
+      @feeds = Feed.find(:all, :limit => 20, :order => "created_at DESC")
+      CACHE.set("welcome_feeds", @feeds, 60*60)
+    end
     
     # Get english rss from cache or from net
     unless @rss_english = CACHE.get("rss_english")
@@ -30,13 +34,21 @@ class WelcomeController < ApplicationController
     unless @rss_spanish = CACHE.get("rss_spanish")
       begin
         @rss_spanish = SimpleRSS.parse open('http://bitacora.feevy.com/feed/')
-        CACHE.set("rss_spanish", @rss_english, 60*60)
+        CACHE.set("rss_spanish", @rss_spanish, 60*60)
       rescue
         @rss_spanish = []
       end
     end
-    @nb_users = User.count
-    @nb_feeds = Feed.count
+    
+    unless @nb_users = CACHE.get("nb_users")
+      @nb_users = User.count
+      CACHE.set("nb_users", @nb_users, 60*60)
+    end
+    
+    unless @nb_feeds = CACHE.get("nb_feeds")
+      @nb_feeds = Feed.count
+      CACHE.set("nb_feeds", @nb_feeds, 60*60)
+    end
   end
   
   def coming_soon
