954c21aedb349407a526c8a17c569e924fadfdd9
wwwfeev <wwwfeev@2117fd12-192b-0410-bd5d-90befd50b342>
2007-06-16 11:16:22 +0000

    Add spanish feevy blog and add cache for rss
    git-svn-id: svn+ssh://64.22.96.6/var/www/apps/feevy/repos/trunk@198 2117fd12-192b-0410-bd5d-90befd50b342

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -15,10 +15,25 @@ class WelcomeController < ApplicationController
   
   def welcome
     @feeds = Feed.find(:all, :limit => 20, :order => "created_at DESC")
-    begin
-      @rss = SimpleRSS.parse open('http://blog.feevy.com/feed/')
-    rescue
-      @rss = []
+    
+    # Get english rss from cache or from net
+    unless @rss_english = CACHE.get("rss_english")
+      begin
+        @rss_english = SimpleRSS.parse open('http://blog.feevy.com/feed/')
+        CACHE.set("rss_english", @rss_english, 60*60)
+      rescue
+        @rss_english = []
+      end
+    end
+    
+    # Get spanish rss from cache or from net
+    unless @rss_spanish = CACHE.get("rss_spanish")
+      begin
+        @rss_spanish = SimpleRSS.parse open('http://bitacora.feevy.com/feed/')
+        CACHE.set("rss_spanish", @rss_english, 60*60)
+      rescue
+        @rss_spanish = []
+      end
     end
     @nb_users = User.count
     @nb_feeds = Feed.count
