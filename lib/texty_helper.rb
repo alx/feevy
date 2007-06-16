174ff20c09d03794b15a8b82f5facd19b8dc9861
wwwfeev <wwwfeev@2117fd12-192b-0410-bd5d-90befd50b342>
2007-06-16 11:47:02 +0000

    add more decoding rules
    git-svn-id: svn+ssh://64.22.96.6/var/www/apps/feevy/repos/trunk@201 2117fd12-192b-0410-bd5d-90befd50b342

--- lib/texty_helper.rb
+++ lib/texty_helper.rb
@@ -72,12 +72,15 @@ module TextyHelper
         if encoding == 'utf-8'
           
           # Some strange caracters to handle
-          text.gsub!("\342\200\176", "\'") # single quote
-          text.gsub!("\342\200\177", "\'") # single quote
-          text.gsub!("\342\200\230", "\'") # single quote
-          text.gsub!("\342\200\231", "\'") # single quote
-          text.gsub!("\342\200\234", "\"") # Double quote, right
-          text.gsub!("\342\200\235", "\"") # Double quote, left
+          text.gsub!("\342\200\042", "&ndash;") # en-dash
+          text.gsub!("\342\200\041", "&mdash;") # em-dash
+          text.gsub!("\342\200\174", "&hellip;") # elipse
+          text.gsub!("\342\200\176", "&lsquo;") # single quote
+          text.gsub!("\342\200\177", "&rsquo;") # single quote
+          text.gsub!("\342\200\230", "&rsquo;") # single quote
+          text.gsub!("\342\200\231", "&rsquo;") # single quote
+          text.gsub!("\342\200\234", "&ldquo;") # Double quote, right
+          text.gsub!("\342\200\235", "&rdquo;") # Double quote, left
           text.gsub!("\342\200\242", ".")
           text.gsub!("\342\202\254", "&euro;"); # Euro symbol
           text.gsub!(/\S\200\S/, " ") # every strange character send to the moon
@@ -87,7 +90,21 @@ module TextyHelper
           text.gsub!("\231", "\'") # single quote
           text.gsub!("\234", "\"") # Double quote
           text.gsub!("\235", "\"") # Double quote
-          text.gsub!("\223", "-") # Long horizontal bar
+          text.gsub!("\205", "-");		# ISO-Latin1 horizontal elipses (0x85)
+          text.gsub!("\221", "\'");	# ISO-Latin1 left single-quote
+          text.gsub!("\222", "\'");	# ISO-Latin1 right single-quote
+          text.gsub!("\223", "\"");	# ISO-Latin1 left double-quote
+          text.gsub!("\224", "\"");	# ISO-Latin1 right double-quote
+          text.gsub!("\225", "\*");	# ISO-Latin1 bullet
+          text.gsub!("\226", "-");		# ISO-Latin1 en-dash (0x96)
+          text.gsub!("\227", "-");		# ISO-Latin1 em-dash (0x97)
+          text.gsub!("\230", "~");		# ISO-Latin1 small tilde
+          text.gsub!("\233", ">");		# ISO-Latin1 single right angle quote
+          text.gsub!("\240", " ");		# ISO-Latin1 nonbreaking space
+          text.gsub!("\246", "\|");	# ISO-Latin1 broken vertical bar
+          text.gsub!("\255", "");		# ISO-Latin1 soft hyphen (0xAD)
+          text.gsub!("\264", "\'");	# ISO-Latin1 spacing acute
+          text.gsub!("\267", "\*");	# ISO-Latin1 middle dot (0xB7)
           ic = Iconv.new('UTF-8//IGNORE', 'UTF-8')
           text = ic.iconv(text + ' ')[0..-2]
         elsif encoding == 'iso-8859-15'
