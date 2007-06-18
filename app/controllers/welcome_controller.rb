c8f9d6917095fe49a84a051df35e0b017eb7382d
wwwfeev <wwwfeev@2117fd12-192b-0410-bd5d-90befd50b342>
2007-06-18 21:37:44 +0000

    Add new cached welcome page
    git-svn-id: svn+ssh://64.22.96.6/var/www/apps/feevy/repos/trunk@206 2117fd12-192b-0410-bd5d-90befd50b342

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -16,16 +16,25 @@ class WelcomeController < ApplicationController
   def welcome
     
     unless @feeds = CACHE.get("welcome_feeds")
-      @feeds = Feed.find(:all, :limit => 20, :order => "created_at DESC")
+      @feeds = ""
+      Feed.find(:all, :limit => 20, :order => "created_at DESC", :conditions => ["title IS NOT NULL"]).each do |feed|
+        @feeds << "<a href='#{feed.href}'>#{feed.title}</a><br/>"
+      end
       CACHE.set("welcome_feeds", @feeds, 60*60)
     end
     
     # Get english rss from cache or from net
     unless @rss_english = CACHE.get("rss_english")
       begin
-        @rss_english = SimpleRSS.parse(open('http://blog.feevy.com/feed/'))
+        feed_item = Hpricot(open('http://blog.feevy.com/feed/')).search("item").first
+        pubDate = Time.parse((feed_item/"pubDate").inner_text)
+        @rss_english = "<p>#{pubDate.day}.#{pubDate.month}.#{pubDate.year}<p><br/>"
+        @rss_english << "<h4>" << (feed_item/"title").inner_text << "</h4><br/>"
+        @rss_english << "<h5>" << (feed_item/"description").inner_text
+        @rss_english << "<a href=" << (feed_item/"link").inner_text << ">continue</a>.</h5><br/><br/>"
         CACHE.set("rss_english", @rss_english, 60*60)
-      rescue
+      rescue => err
+        logger.debug "Error while reading english blog feed: #{err}"
         @rss_english = []
       end
     end
@@ -33,9 +42,15 @@ class WelcomeController < ApplicationController
     # Get spanish rss from cache or from net
     unless @rss_spanish = CACHE.get("rss_spanish")
       begin
-        @rss_spanish = SimpleRSS.parse(open('http://bitacora.feevy.com/feed/'))
+        feed_item = Hpricot(open('http://bitacora.feevy.com/feed/')).search("item").first
+        pubDate = Time.parse((feed_item/"pubDate").inner_text)
+        @rss_spanish = "<p>#{pubDate.day}.#{pubDate.month}.#{pubDate.year}<p><br/>"
+        @rss_spanish << "<h4>" << (feed_item/"title").inner_text << "</h4><br/>"
+        @rss_spanish << "<h5>" << (feed_item/"description").inner_text
+        @rss_spanish << "<a href=" << (feed_item/"link").inner_text << ">continue</a>.</h5><br/><br/>"
         CACHE.set("rss_spanish", @rss_spanish, 60*60)
-      rescue
+      rescue => err
+        logger.debug "Error while reading spanish blog feed: #{err}"
         @rss_spanish = []
       end
     end
