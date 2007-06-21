821e34fc3e1e9251177ac85cd96622e9073ad2ba
wwwfeev <wwwfeev@2117fd12-192b-0410-bd5d-90befd50b342>
2007-06-21 15:00:41 +0000

    Add special rule for el pais
    git-svn-id: svn+ssh://64.22.96.6/var/www/apps/feevy/repos/trunk@207 2117fd12-192b-0410-bd5d-90befd50b342

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -181,6 +181,8 @@ class Feed < ActiveRecord::Base
       elsif self.href =~ /\.blogsome\.com/
         rss_link  = doc.search("//link[@type='text/xml']").to_s.scan(/href=['"]?([^'"]*)['" ]/)
         rss_link = rss_link[0].to_s if rss_link.is_a? Array
+      elsif self.href =~ /\.elpais\.com/
+        rss_link = self.href << "/blog-comunidad/posts.rss"
       else
         rss_link  = doc.search("//link[@type='application/rss+xml']").to_s.scan(/href=['"]?([^'"]*)['" ]/)
         rss_link = rss_link[0].to_s if rss_link.is_a? Array
