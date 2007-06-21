3897b6a54268100b70670b8bb0cd5a5734a29c61
wwwfeev <wwwfeev@2117fd12-192b-0410-bd5d-90befd50b342>
2007-06-21 16:05:58 +0000

    correct el pais rss header reading
    git-svn-id: svn+ssh://64.22.96.6/var/www/apps/feevy/repos/trunk@213 2117fd12-192b-0410-bd5d-90befd50b342

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -182,12 +182,12 @@ class Feed < ActiveRecord::Base
         rss_link  = doc.search("//link[@type='text/xml']").to_s.scan(/href=['"]?([^'"]*)['" ]/)
         rss_link = rss_link[0].to_s if rss_link.is_a? Array
       elsif self.href =~ /\.elpais\.com/
-        if self.href !~ /posts$/
+        if self.href =~ /posts$/
           rss_link = self.href << ".rss"
-        elsif self.href !~ /\/$/
-          rss_link = self.href << "/posts.rss"
-        else
+        elsif self.href =~ /\/$/
           rss_link = self.href << "posts.rss"
+        else
+          rss_link = self.href << "/posts.rss"
         end
       else
         rss_link  = doc.search("//link[@type='application/rss+xml']").to_s.scan(/href=['"]?([^'"]*)['" ]/)
