60db7e7f53d30e83883bbf0deb87af5cc43e7985
wwwfeev <wwwfeev@2117fd12-192b-0410-bd5d-90befd50b342>
2007-11-15 08:47:59 +0000

    Add method to remove duplicates
    git-svn-id: svn+ssh://64.22.96.6/var/www/apps/feevy/repos/trunk@382 2117fd12-192b-0410-bd5d-90befd50b342

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -8,4 +8,22 @@ class Subscription < ActiveRecord::Base
   def feevy_avatar_url
     self.avatar.url
   end
+  
+  def remove_duplicates
+    nb_duplicates = 0
+    subscriptions = Subscription.find(:all)
+    subscriptions.each do |subscription|
+       duplicates = subs.select{|duplicate| duplicate.id != subscription.id 
+                                            and duplicate.user_id == subscription.user_id 
+                                            and duplicate.feed_id == subscription.feed_id}
+       duplicates.each do |duplicate|
+         logger.debug "Duplicate found: #{duplicate.id}"
+         nb_duplicates += 1
+         #duplicate.destroy
+         subscriptions.delete(duplicate)
+       end
+       subscriptions.delete(subscription)
+    end
+    logger.info "Duplicates found: #{nb_duplicates}"
+  end
 end
