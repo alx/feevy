b3189c41e548f36d1ae4d3c2b0c8266d9433fc27
Alexandre Girard <alx.girard@gmail.com>
2008-11-24 18:41:33 +0100

    Add timeout when reading feeds from homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -23,11 +23,17 @@ class WelcomeController < ApplicationController
       CACHE.set("welcome_feeds", @feeds, 60*60)
     end
     
+    delay = 30
+    
     # Get english rss from cache or from net
     unless @rss_english = CACHE.get("rss_english")
       begin
-        @rss_english = show_post Hpricot(open('http://blog.feevy.com/feed/')).search("item").first
-        CACHE.set("rss_english", @rss_english, 60*60)
+        Timeout::timeout(delay) {
+          @rss_english = show_post Hpricot(open('http://blog.feevy.com/feed/')).search("item").first
+        }
+      rescue Timeout::Error
+        logger.debug "Timeout while reading english blog feed: #{err}"
+        @rss_english = []
       rescue => err
         logger.debug "Error while reading english blog feed: #{err}"
         @rss_english = []
@@ -37,13 +43,20 @@ class WelcomeController < ApplicationController
     # Get spanish rss from cache or from net
     unless @rss_spanish = CACHE.get("rss_spanish")
       begin
-        @rss_spanish = show_post Hpricot(open('http://bitacora.feevy.com/feed/')).search("item").first
-        CACHE.set("rss_spanish", @rss_spanish, 60*60)
+        Timeout::timeout(delay) {
+          @rss_spanish = show_post Hpricot(open('http://bitacora.feevy.com/feed/')).search("item").first
+        }
+      rescue Timeout::Error
+        logger.debug "Timeout while reading spanish blog feed: #{err}"
+        @rss_spanish = []
       rescue => err
         logger.debug "Error while reading spanish blog feed: #{err}"
         @rss_spanish = []
       end
     end
+    
+    CACHE.set("rss_spanish", @rss_spanish, 60*60)
+    CACHE.set("rss_english", @rss_english, 60*60)
     
     unless @nb_users = CACHE.get("nb_users")
       @nb_users = User.count
